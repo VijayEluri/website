8e0604f97dacbe956543999b515ddfc2a2439d99
jeremyd <jeremyd@jeremyd-laptop>
2010-12-09 12:57:27 -0700

    initial commit

---
+++ org.oobium.www.update_site/src/org/oobium/www/update_site/Activator.java
@@ -0,0 +1,19 @@
+package org.oobium.www.update_site;
+
+import org.oobium.app.ModuleService;
+import org.oobium.app.server.routing.Router;
+import org.oobium.utils.Config;
+
+public class Activator extends ModuleService {
+
+	@Override
+	public void addRoutes(Config config, Router router) {
+		router.addAssetRoutes(this);
+	}
+
+	@Override
+	public void removeRoutes(Config config, Router router) {
+		router.removeAssetRoutes(this);
+	}
+
+}
---
+++ org.oobium.www.update_site/src/org/oobium/www/update_site/configuration.js
@@ -0,0 +1,15 @@
+({
+
+dev: {
+	// TODO: add your development mode configuration
+},
+
+test: {
+	// TODO: add your test mode configuration
+},
+
+prod: {
+	// TODO: add your production mode configuration
+}
+
+});
