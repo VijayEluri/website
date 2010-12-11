c9cfd9fca5144cd4c1dbeac55365262bf4273d7f
toris <toris@toris-laptop>
2010-12-11 14:56:26 -0700

    added manager to DEV build

--- org.oobium.www/src/org/oobium/www/configuration.js
+++ org.oobium.www/src/org/oobium/www/configuration.js
@@ -10,7 +10,7 @@ modules: "org.oobium.www.update_site_1.0.0",
 dev: {
 	host: "localhost",
 	port: 5000,
-	modules: "org.oobium.app.dev_0.5.0",
+	modules: [ "org.oobium.app.dev_0.5.0", "org.oobium.manager_0.5.0" ]
 },
 
 test: {
