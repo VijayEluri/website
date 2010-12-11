514b0bb91d7ce9aac2a0df840106a1339d86f68a
toris <toris@toris-laptop>
2010-12-11 14:56:40 -0700

    added route for Styles

--- org.oobium.www/src/org/oobium/www/Activator.java
+++ org.oobium.www/src/org/oobium/www/Activator.java
@@ -3,12 +3,14 @@ package org.oobium.www;
 import org.oobium.app.AppService;
 import org.oobium.app.server.routing.Router;
 import org.oobium.utils.Config;
+import org.oobium.www.views._layouts.Styles;
 
 public class Activator extends AppService {
 
 	@Override
 	public void addRoutes(Config config, Router router) {
 		router.addAssetRoutes(this);
+		router.addRoute(Styles.class);
 	}
 
 }
